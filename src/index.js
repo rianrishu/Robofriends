b0c59b7390d6ad78bf430c6e084fd9ba70f1963c
Rishu <rishuanand2552@gmail.com>
2020-12-04 14:16:00 +0530

    'initial'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 import 'tachyons';
 import CardList from './CardList';
 
