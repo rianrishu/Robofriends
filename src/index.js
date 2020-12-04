e4567ca55c5301ae3b68ffd7df62098b06ad150c
Rishu <rishuanand2552@gmail.com>
2020-12-04 14:11:23 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import CardList from './CardList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
